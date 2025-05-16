27495f356eb596f7d62dc1ac2bdfe48ea30d983f
sahilrana52155 <rana52155@gmail.com>
2025-05-16 17:47:15 +0530

    Fix routing by adding basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/storecontext.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/Food-Delivery-website">
         <StoreContextProvider>
             <App />
         </StoreContextProvider>
